858cbe367e550c03407fc15ea51538c2b0c05095
Matthew Salerno <matthewsalern@gmail.com>
2013-09-04 20:48:05 -0400

    hello world from app delegate

--- app/app_delegate.rb
+++ app/app_delegate.rb
@@ -1,5 +1,8 @@
 class AppDelegate
   def application(application, didFinishLaunchingWithOptions:launchOptions)
+    @window = UIWindow.alloc.initWithFrame(UIScreen.mainScreen.bounds)
+    @window.makeKeyAndVisible
+    App.alert("Hello from BestSeller!")
     true
   end
 end
